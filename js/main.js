84957bff4a7485b083e80ac291ccf3800e767775
temirovshahobiddin <temirovshahobiddin@gmail.com>
2022-06-24 19:51:53 +0500

    changing

--- js/main.js
+++ js/main.js
@@ -6,3 +6,15 @@ navMenu.addEventListener("click", function () {
     navList.classList.toggle("active")
     
 })
+
+
+let headerSlide = document.querySelectorAll(".header-slide")
+
+for (let i = 0; i < headerSlide.length; i++) {
+	headerSlide[i].addEventListener("click", function (e) {
+		for (let k = 0; k < headerSlide.length; k++) {
+			headerSlide[k].classList.remove("active")
+		}
+		headerSlide[i].classList.add("active")
+	})
+}
